bc9b34de295be0cee00b36802e8f6d0be676cf8e
Biswajit Patra <patrabiswajit133@gmail.com>
2021-12-26 20:08:27 +0530

    Remove server

--- client/pages/api/[...catchAll].js
+++
@@ -1,9 +0,0 @@
-// import { Backend } from "../../backend/main";
-// import { NextApiRequest, NextApiResponse } from "next";
-
-// export default (req, res) =>
-//   new Promise(async (resolve) => {
-//     const listener = await Backend.getListener();
-//     listener(req, res);
-//     res.on("finish", resolve);
-//   });
